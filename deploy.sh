f9d86ef81e66a25a21bafb49d07f18200beef7f4
easyVuejs <jiangnan3948@gmail.com>
2022-10-14 14:29:19 +0800

    feat: sh test

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # 构建
-npm run build
+yarn build
 
 # 进入待发布的目录
 cd docs/.vitepress/dist
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果部署到 https://<USERNAME>.github.io
-git push -f git@github.com:easyVuejs/vitepress-doc4components.git gh-pages
+git push -f git@github.com:easyVuejs/vitepress-doc4components.git main:gh-pages
 
 # 如果是部署到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,26 +1,26 @@
 module.exports = {
-  title: "geely-business-components",
-  base: "./",
+  title: 'geely-business-components',
+  base: '/vitepress-doc4components/',
   themeConfig: {
     nav: [
-      { text: "首页", link: "/" },
+      { text: '首页', link: '/' },
       {
-        text: "指南",
-        link: "/guide/",
+        text: '指南',
+        link: '/guide/',
         items: [
-          { text: "介绍", link: "/guide/introduce/" },
-          { text: "快速上手", link: "/guide/quickstart/" },
-          { text: "目录结构", link: "/guide/catalogue/" },
+          { text: '介绍', link: '/guide/introduce/' },
+          { text: '快速上手', link: '/guide/quickstart/' },
+          { text: '目录结构', link: '/guide/catalogue/' },
         ],
       },
       {
-        text: "组件",
-        link: "/components/",
+        text: '组件',
+        link: '/components/',
         items: [
-          { text: "查询组件", link: "/components/GeelySearch/" },
-          { text: "卡片组件", link: "/components/GeelyCard/" },
-          { text: "树组件", link: "/components/GeelyTree/" },
-          { text: "组合查询组件", link: "/components/GeelySelectSearch/" },
+          { text: '查询组件', link: '/components/GeelySearch/' },
+          { text: '卡片组件', link: '/components/GeelyCard/' },
+          { text: '树组件', link: '/components/GeelyTree/' },
+          { text: '组合查询组件', link: '/components/GeelySelectSearch/' },
         ],
       },
     ],
@@ -29,17 +29,17 @@ module.exports = {
     //   indexName: 'index_name'
     // },
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
-    logo: "/img/hero.png", // 导航栏logo
-    repo: "xugaoyi/geely-business-components", // 导航栏右侧生成Github链接
+    logo: '/img/hero.png', // 导航栏logo
+    repo: 'https://easyvuejs.github.io/vitepress-doc4components/', // 导航栏右侧生成Github链接
     searchMaxSuggestions: 10, // 搜索结果显示最大数
-    lastUpdated: "上次更新", // 更新的时间，及前缀文字   string | boolean (取值为git提交时间)
+    lastUpdated: '上次更新', // 更新的时间，及前缀文字   string | boolean (取值为git提交时间)
 
     // docsDir: 'docs', // 编辑的文件夹
     // editLinks: true, // 编辑链接
     // editLinkText: '编辑',
 
     // 以下配置是Vdoing主题改动的和新增的配置
-    sidebar: { mode: "structuring", collapsable: false }, // 侧边栏  'structuring' | { mode: 'structuring', collapsable: Boolean} | 'auto' | 自定义    温馨提示：目录页数据依赖于结构化的侧边栏数据，如果你不设置为'structuring',将无法使用目录页
+    sidebar: { mode: 'structuring', collapsable: false }, // 侧边栏  'structuring' | { mode: 'structuring', collapsable: Boolean} | 'auto' | 自定义    温馨提示：目录页数据依赖于结构化的侧边栏数据，如果你不设置为'structuring',将无法使用目录页
 
     // sidebarOpen: false, // 初始状态是否打开侧边栏，默认true
     updateBar: {
@@ -53,7 +53,7 @@ module.exports = {
     //   '图标地址2'
     // ],
 
-    pageStyle: "line", // 页面风格，可选值：'card'卡片 | 'line' 线（未设置bodyBgImg时才生效）， 默认'card'。 说明：card时背景显示灰色衬托出卡片样式，line时背景显示纯色，并且部分模块带线条边框
+    pageStyle: 'line', // 页面风格，可选值：'card'卡片 | 'line' 线（未设置bodyBgImg时才生效）， 默认'card'。 说明：card时背景显示灰色衬托出卡片样式，line时背景显示纯色，并且部分模块带线条边框
 
     // contentBgStyle: 1,
 
@@ -63,14 +63,14 @@ module.exports = {
 
     author: {
       // 文章默认的作者信息，可在md文件中单独配置此信息 String | {name: String, href: String}
-      name: "Nan Jiang", // 必需
-      href: "https://github.com/", // 可选的
+      name: 'Nan Jiang', // 必需
+      href: 'https://github.com/', // 可选的
     },
   },
   markdown: {
     config: (md) => {
       const { demoBlockPlugin } = require('vitepress-theme-demoblock');
       md.use(demoBlockPlugin);
-    }
-  }
+    },
+  },
 };
